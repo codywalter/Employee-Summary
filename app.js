c5e8166a9a741ca3382735111f198286b08bc49b
Cody Walter <cody.walter@providencecc.edu>
2020-09-07 22:35:56 -0700

    updatted app.js to properly run inquirer prompts

--- app.js
+++ app.js
@@ -10,26 +10,149 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
+const team = [];
 
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
+function employeeInformation() {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        message: "What type of employee would ypu like to add?",
+        name: "name",
+        choices: ["Manager", "Engineer", "Intern", "Show Summary"],
+      },
+    ])
+    .then((val) => {
+      if (val.name === "Manager") {
+        managerInformation();
+      } else if (val.name === "Engineer") {
+        engineerInformation();
+      } else if (val.name === "Intern") {
+        internInformation();
+      } else if (val.name === "Show Summary") {
+        generateHTML(outputPath, render(team));
+      }
+    });
+} // END FUNCTION
 
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
+function managerInformation() {
+  return inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "What is your manager's name?",
+        name: "name",
+      },
+      {
+        type: "input",
+        message: "What is your manager's id number?",
+        name: "id",
+      },
+      {
+        type: "input",
+        message: "What is your manager's email?",
+        name: "email",
+      },
+      {
+        type: "input",
+        message: "What is your manager's office number?",
+        name: "number",
+      },
+    ])
+    .then(function (answer) {
+      let manager = new Manager(
+        answer.name,
+        answer.id,
+        answer.email,
+        answer.number
+      );
+      team.push(manager);
 
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
+      employeeInformation();
+    });
+} // END FUNCTION
 
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
+function engineerInformation() {
+  return inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "What is your engineer's name?",
+        name: "name",
+      },
+      {
+        type: "input",
+        message: "What is your engineer's id number?",
+        name: "id",
+      },
+      {
+        type: "input",
+        message: "What is your engineer's email?",
+        name: "email",
+      },
+      {
+        type: "input",
+        message: "What is your engineer's GitHub username?",
+        name: "GitHub",
+      },
+    ])
+    .then(function (answer) {
+      let engineer = new Engineer(
+        answer.name,
+        answer.id,
+        answer.email,
+        answer.GitHub
+      );
+      team.push(engineer);
 
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
+      employeeInformation();
+    });
+} // END FUNCTION
+
+function internInformation() {
+  return inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "What is your intern's name?",
+        name: "name",
+      },
+      {
+        type: "input",
+        message: "What is your intern's id number?",
+        name: "id",
+      },
+      {
+        type: "input",
+        message: "What is your intern's email?",
+        name: "email",
+      },
+      {
+        type: "input",
+        message: "Where does your intern go to school?",
+        name: "school",
+      },
+    ])
+    .then(function (answer) {
+      let intern = new Intern(
+        answer.name,
+        answer.id,
+        answer.email,
+        answer.school
+      );
+      team.push(intern);
+
+      employeeInformation();
+    });
+} // END FUNCTION
+
+function generateHTML(fileName, data) {
+  fs.writeFile(fileName, data, "utf8", function (err) {
+    if (err) {
+      throw err;
+    }
+    console.log("You have successfully written your employee summary!");
+  });
+}
+
+employeeInformation();
